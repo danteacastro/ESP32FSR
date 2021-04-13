7284870fc861041c4cf0f4383b85d758a389388f
danteacastro <danteacastro@gmail.com>
2021-04-13 10:55:13 -0500

    Update esp32.ino

--- esp32.ino
+++ esp32.ino
@@ -1,13 +1,19 @@
+#define BLYNK_PRINT Serial 
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
-char auth[] = "Blynk Auth Code goes here";
-char ssid[] = "WiFi name";
-char pass[] = "WiFi password";
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = " S_Dhdv9XltboGCO159Mr1WCXJXSriE_E";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "ATTGjg27Ea";
+char pass[] = "p?8fdbwvv5n4";
 
 
-float cf = 5; // caliberation factor
+float cf = 5; // calibration factor
 const int ffs1 = 25; //  
 const int ffs2 = 33; //
 const int ffs3 = 34; //
@@ -19,8 +25,40 @@ int ffsdata2 = 0;
 int ffsdata3 = 0;
 int ffsdata4 = 0;
 float vout,vout1,vout2,vout3,vout4;
+
+float value;
+float value1;
+float value2;
+float value3;
+float value4;
+BLYNK_WRITE(V0)
+{
+  value = param.asFloat(); // Get value as float
+}
+
+BLYNK_WRITE(V1)
+{
+  value1 = param.asFloat(); // Get value as float
+}
+
+BLYNK_WRITE(V2)
+{
+  value2 = param.asFloat(); // Get value as float
+}
+
+BLYNK_WRITE(V3)
+{
+  value3 = param.asFloat(); // Get value as float
+}
+
+BLYNK_WRITE(V4)
+{
+  value4 = param.asFloat(); // Get value as float
+}
+
 void setup()
 {
+
   Blynk.begin(auth, ssid, pass);
   Serial.begin(115200);
   pinMode(ffs1, INPUT);
@@ -32,6 +70,7 @@ void setup()
 
 void loop()
 {
+  Blynk.run();
 int ffsdata = 0;
 int ffsdata1 = 0;
 int ffsdata2 = 0;
@@ -68,13 +107,16 @@ if((vout > 0.0f) || (vout1 > 0.0f) || (vout2 > 0.0f) || (vout3 > 0.0f) || (vout4
    Serial.print(vout3);
    Serial.print(",");
    Serial.print(vout4);
-   Serial.println("");
-   Blynk.virtualWrite(V0, vout);    
-   Blynk.virtualWrite(V1, vout1);  
-   Blynk.virtualWrite(V2, vout2);  
-   Blynk.virtualWrite(V3, vout3);  
-   Blynk.virtualWrite(V4, vout4);  
-           
+   Serial.println("");  
+ 
+
+               Blynk.virtualWrite(V0, float(vout));
+               Blynk.virtualWrite(V1, float(vout1));
+               Blynk.virtualWrite(V2, float(vout2));
+               Blynk.virtualWrite(V3, float(vout3));
+               Blynk.virtualWrite(V4, float(vout4));
+               
+               
 }
 delay(100);
 }
